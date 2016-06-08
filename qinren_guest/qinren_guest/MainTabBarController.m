7500192afe3c0d8302a9862d067a7195a30d4f4f
yang <wuhan_qinren@aaaas-imac.local>
2016-06-08 09:54:01 +0800

    “封装子视图”

--- qinren_guest/qinren_guest/MainTabBarController.m
+++ qinren_guest/qinren_guest/MainTabBarController.m
@@ -10,6 +10,8 @@
 
 @interface MainTabBarController ()
 
+-(void)setUpVc:(UIViewController *)vc setImage:(NSString *)normal setSelectImage:(NSString *)select setTitle:(NSString *)title;
+
 @end
 
 @implementation MainTabBarController
@@ -33,82 +35,15 @@
     
     [item setTitleTextAttributes:SelectAttrs forState:UIControlStateSelected];
     
-    UIViewController *vc1;
-    vc1= [[UIViewController alloc]init];
+    [self setUpVc:[[UIViewController alloc]init] setImage:@"tab_bar_Image_index_noselect" setSelectImage:@"tab_bar_Image_index_select" setTitle:@"主页"];
     
-    vc1.view.backgroundColor = [UIColor redColor];
+    [self setUpVc:[[UIViewController alloc]init] setImage:@"tab_bar_Image_exchange_noselect" setSelectImage:@"tab_bar_Image_exchange_select" setTitle:@"交流群"];
     
-    UINavigationController *nav1;
-    nav1 = [[UINavigationController alloc]initWithRootViewController:vc1];
+    [self setUpVc:[[UIViewController alloc]init] setImage:@"tab_bar_Image_shopcar_noselect" setSelectImage:@"tab_bar_Image_shopcar_select" setTitle:@"购物车"];
     
-    vc1.navigationItem.title = @"主页";
+    [self setUpVc:[[UIViewController alloc]init] setImage:@"tab_bar_Image_my_noselect" setSelectImage:@"tab_bar_Image_my_select" setTitle:@"我的"];
     
-    vc1.tabBarItem.title = @"主页";
-    
-    vc1.tabBarItem.image = [UIImage imageNamed:@"tab_bar_Image_index_noselect"];
-    
-    vc1.tabBarItem.selectedImage = [UIImage imageNamed:@"tab_bar_Image_index_select"];
-    
-    [self addChildViewController:nav1];
-    
-    
-    UIViewController *vc2;
-    vc2= [[UIViewController alloc]init];
-    
-    vc2.view.backgroundColor = [UIColor yellowColor];
-    
-    UINavigationController *nav2;
-    nav2 = [[UINavigationController alloc]initWithRootViewController:vc2];
-    
-    vc2.navigationItem.title = @"交流群";
-    
-    vc2.tabBarItem.title = @"交流群";
-    
-    vc2.tabBarItem.image = [UIImage imageNamed:@"tab_bar_Image_exchange_noselect"];
-    
-    vc2.tabBarItem.selectedImage = [UIImage imageNamed:@"tab_bar_Image_exchange_select"];
-    
-    [self addChildViewController:nav2];
-    
-    
-    UIViewController *vc3;
-    vc3= [[UIViewController alloc]init];
-    
-    vc3.view.backgroundColor = [UIColor blueColor];
-    
-    UINavigationController *nav3;
-    nav3 = [[UINavigationController alloc]initWithRootViewController:vc3];
-    
-    vc3.navigationItem.title = @"购物车";
-    
-    vc3.tabBarItem.title = @"购物车";
-    
-    vc3.tabBarItem.image = [UIImage imageNamed:@"tab_bar_Image_shopcar_noselect"];
-    
-    vc3.tabBarItem.selectedImage = [UIImage imageNamed:@"tab_bar_Image_shopcar_select"];
-    
-    [self addChildViewController:nav3];
-    
-    
-    UIViewController *vc4;
-    vc4= [[UIViewController alloc]init];
-    
-    vc4.view.backgroundColor = [UIColor greenColor];
-    
-    UINavigationController *nav4;
-    nav4 = [[UINavigationController alloc]initWithRootViewController:vc4];
-    
-    vc4.navigationItem.title = @"我的";
-    
-    vc4.tabBarItem.title = @"我的";
-    
-    vc4.tabBarItem.image = [UIImage imageNamed:@"tab_bar_Image_my_noselect"];
-    
-    vc4.tabBarItem.selectedImage = [UIImage imageNamed:@"tab_bar_Image_my_select"];
-    
-    [self addChildViewController:nav4];
-    
-}
+  }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -124,5 +59,27 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+
+/*封装子视图*/
+
+-(void)setUpVc:(UIViewController *)vc setImage:(NSString *)normal setSelectImage:(NSString *)select setTitle:(NSString *)title
+
+{
+    vc.tabBarItem.title = title;
+    
+    vc.tabBarItem.image = [UIImage imageNamed:normal];
+    
+    vc.tabBarItem.selectedImage = [UIImage imageNamed:select];
+    
+    vc.view.backgroundColor = [UIColor colorWithRed:arc4random_uniform(255)/255.0 green:arc4random_uniform(255)/255.0 blue:arc4random_uniform(255)/255.0 alpha:1];
+    
+    [self addChildViewController:vc];
+
+    
+}
+
+
+
 
 @end
