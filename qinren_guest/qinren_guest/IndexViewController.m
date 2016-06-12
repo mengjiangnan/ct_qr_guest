8c8f04da7884ddf9035ab5bd71d95a8b6e26f100
yang <wuhan_qinren@aaaas-imac.local>
2016-06-12 14:57:38 +0800

    “loop view”

--- qinren_guest/qinren_guest/IndexViewController.m
+++ qinren_guest/qinren_guest/IndexViewController.m
@@ -22,27 +22,25 @@
     demoContainerView.contentSize = CGSizeMake(self.view.frame.size.width, 1200);
     [self.view addSubview:demoContainerView];
     
-    self.title = @"轮播Demo";
+    //self.title = @"轮播Demo";
     
     // 情景一：采用本地图片实现
     NSArray *imageNames;
-    imageNames= @[          @"h1.jpg",
-                            @"h2.jpg",
-                            @"h3.jpg",
-                            @"h4.jpg",
-                            
-                            ];
+    imageNames= @[@"h1.jpg",@"h2.jpg",@"h3.jpg",@"h4.jpg",];
     
     CGFloat w;
     w = self.view.bounds.size.width;
     
     // 本地加载 --- 创建不带标题的图片轮播器
     
-    SDCycleScrollView *cycleScrollView = [SDCycleScrollView cycleScrollViewWithFrame:CGRectMake(0, 64, w, 180) shouldInfiniteLoop:YES imageNamesGroup:imageNames];
+    SDCycleScrollView *cycleScrollView = [SDCycleScrollView cycleScrollViewWithFrame:CGRectMake(0, 0, w, 180) shouldInfiniteLoop:YES imageNamesGroup:imageNames];
     cycleScrollView.delegate = self;
     cycleScrollView.pageControlStyle = SDCycleScrollViewPageContolStyleAnimated;
     [demoContainerView addSubview:cycleScrollView];
-    cycleScrollView.scrollDirection = UICollectionViewScrollDirectionVertical;
+    
+    //     －－－垂直滚动－－－
+    
+    //cycleScrollView.scrollDirection = UICollectionViewScrollDirectionVertical;
     
     //         --- 轮播时间间隔，默认1.0秒，可自定义
     
