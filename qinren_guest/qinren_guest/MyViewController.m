63b2add65aa7ddbb357b8998b8ed0be8571057fd
yang <wuhan_qinren@aaaas-imac.local>
2016-06-14 15:47:18 +0800

    "myviewcontroller"

--- qinren_guest/qinren_guest/MyViewController.m
+++ qinren_guest/qinren_guest/MyViewController.m
@@ -15,23 +15,38 @@
 @implementation MyViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor colorWithRed:238.0/255 green:238.0/255 blue:238.0/255 alpha:1.0];
+    
+    //圆形按钮
+    
+    CGFloat myhead_btn_x = self.view.frame.size.width * 0.5 -60;
+    
+    CGFloat myhead_btn_y = 84;
+    
+    UIButton *myhead_btn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    
+    myhead_btn.frame = CGRectMake(myhead_btn_x, myhead_btn_y, 120, 120);
+    
+    //[myhead_btn setTitle:@"启动" forState:UIControlStateNormal];
+    
+    myhead_btn.layer.borderWidth = 1;
+    
+    myhead_btn.layer.borderColor = [UIColor greenColor].CGColor;
+    
+    myhead_btn.layer.cornerRadius = 60;
+    
+    myhead_btn.layer.masksToBounds = YES;
+    
+    //[myhead_btn setTitleColor:[UIColor greenColor]forState:UIControlStateNormal];
+    
+    [myhead_btn setBackgroundImage:[UIImage imageNamed:@"h4"] forState:UIControlStateNormal];
+    
+    [self.view addSubview:myhead_btn];
+     
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
