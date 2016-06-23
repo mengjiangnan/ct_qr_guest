72c2a498ecf6151465d7f86bf5591919c769d3b1
yang <wuhan_qinren@aaaas-imac.local>
2016-06-23 15:54:26 +0800

    "person_config half"

--- qinren_guest/qinren_guest/PersonInformationViewController.m
+++ qinren_guest/qinren_guest/PersonInformationViewController.m
@@ -22,13 +22,31 @@
     
     self.view.backgroundColor = [UIColor colorWithRed:238.0/255 green:238.0/255 blue:238.0/255 alpha:1.0];
     
+    //按钮间的固定高度
+    
+    CGFloat const_height = 5;
+    
+    //按钮固定宽度
+    
+    CGFloat btn_const_width = self.view.frame.size.width;
+    
+    //按钮固定高度
+    
+    CGFloat btn_const_height = self.view.frame.size.height * 0.1;
+    
+    //按钮固定x值
+    
+    CGFloat btn_const_x = 0;
+    
+    //个人信息按钮
+    
     UIButton *my_information_btn = [UIButton buttonWithType:UIButtonTypeSystem];
     
-    CGFloat my_information_btn_x = 0;
+    CGFloat my_information_btn_x = btn_const_x;
     
     CGFloat my_information_btn_y = 84;
     
-    CGFloat my_information_btn_width = self.view.frame.size.width;
+    CGFloat my_information_btn_width = btn_const_width;
     
     CGFloat my_information_btn_height = self.view.frame.size.height * 0.15;
     
@@ -40,17 +58,130 @@
     
     [my_information_btn setImage:[UIImage imageNamed:@"myset_my_head_img"] forState:UIControlStateNormal];
     
-    my_information_btn.imageEdgeInsets = UIEdgeInsetsMake(0, 0, 0, self.view.frame.size.width * 0.2);
+    my_information_btn.imageEdgeInsets = UIEdgeInsetsMake(0, 0, 0, btn_const_height * 0.2);
     
     [my_information_btn setTitle:@"罗忠 手机号:18808680413" forState:UIControlStateNormal];
     
-    my_information_btn.titleLabel.font = [UIFont systemFontOfSize:10.0];
+    my_information_btn.titleLabel.font = [UIFont systemFontOfSize:15.0];
     
     [my_information_btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
     
     [my_information_btn setTitleColor:[UIColor grayColor] forState:UIControlStateHighlighted];
     
     [self.view addSubview:my_information_btn];
+    
+    //个性签名按钮
+    
+    UIButton *signature_btn = [UIButton buttonWithType:UIButtonTypeSystem];
+    
+    CGFloat signature_btn_x = btn_const_x;
+    
+    CGFloat signature_btn_y = my_information_btn_y + my_information_btn.frame.size.height + const_height;
+    
+    CGFloat signature_btn_width = btn_const_width;
+    
+    CGFloat signature_btn_height = btn_const_height;
+    
+    CGRect signature_btn_rect = CGRectMake(signature_btn_x, signature_btn_y, signature_btn_width, signature_btn_height);
+    
+    signature_btn = [[UIButton alloc]initWithFrame:signature_btn_rect];
+    
+    signature_btn.backgroundColor = [UIColor whiteColor];
+    
+    [signature_btn setTitle:@"个性签名:大家发财" forState:UIControlStateNormal];
+    
+    signature_btn.titleLabel.font = [UIFont systemFontOfSize:15.0];
+    
+    [signature_btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    
+    [signature_btn setTitleColor:[UIColor grayColor] forState:UIControlStateHighlighted];
+    
+    [self.view addSubview:signature_btn];
+    
+    //修改密码按钮
+    
+    UIButton *change_pwd_btn = [UIButton buttonWithType:UIButtonTypeSystem];
+    
+    CGFloat change_pwd_btn_x = btn_const_x;
+    
+    CGFloat change_pwd_btn_y = signature_btn_y + btn_const_height + const_height;
+    
+    CGFloat change_pwd_btn_width =btn_const_width;
+    
+    CGFloat change_pwd_btn_height = btn_const_height;
+    
+    CGRect change_pwd_btn_rect = CGRectMake(change_pwd_btn_x, change_pwd_btn_y, change_pwd_btn_width, change_pwd_btn_height);
+    
+    change_pwd_btn = [[UIButton alloc]initWithFrame:change_pwd_btn_rect];
+    
+    change_pwd_btn.backgroundColor = [UIColor whiteColor];
+    
+    [change_pwd_btn setTitle:@"修改密码" forState:UIControlStateNormal];
+    
+    change_pwd_btn.titleLabel.font = [UIFont systemFontOfSize:15.0];
+    
+    [change_pwd_btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    
+    [change_pwd_btn setTitleColor:[UIColor whiteColor] forState:UIControlStateHighlighted];
+    
+    [self.view addSubview:change_pwd_btn];
+    
+    //关于亲仁健康按钮
+    
+    UIButton *about_me_btn = [UIButton buttonWithType:UIButtonTypeSystem];
+    
+    CGFloat about_me_btn_x = btn_const_x;
+    
+    CGFloat about_me_btn_y = change_pwd_btn_y + btn_const_height + const_height;
+    
+    CGFloat about_me_btn_width = btn_const_width;
+    
+    CGFloat about_me_btn_height = btn_const_height;
+    
+    CGRect about_me_btn_rect = CGRectMake(about_me_btn_x, about_me_btn_y, about_me_btn_width, about_me_btn_height);
+    
+    about_me_btn = [[UIButton alloc]initWithFrame:about_me_btn_rect];
+    
+    about_me_btn.backgroundColor = [UIColor whiteColor];
+    
+    [about_me_btn setTitle:@"关于亲仁健康" forState:UIControlStateNormal];
+    
+    about_me_btn.titleLabel.font = [UIFont systemFontOfSize:15.0];
+    
+    [about_me_btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    
+    [about_me_btn setTitleColor:[UIColor grayColor] forState:UIControlStateHighlighted];
+    
+    [self.view addSubview:about_me_btn];
+    
+    //退出登录按钮
+    
+    UIButton *logout_btn = [UIButton buttonWithType:UIButtonTypeSystem];
+    
+    CGFloat logout_btn_x = btn_const_x;
+    
+    CGFloat logout_btn_y = about_me_btn_y + btn_const_height + const_height;
+    
+    CGFloat logout_btn_width = btn_const_width;
+    
+    CGFloat logout_btn_height = btn_const_height;
+    
+    CGRect logout_btn_rect = CGRectMake(logout_btn_x, logout_btn_y, logout_btn_width, logout_btn_height);
+    
+    logout_btn = [[UIButton alloc]initWithFrame:logout_btn_rect];
+    
+    logout_btn.backgroundColor = [UIColor redColor];
+    
+    [logout_btn setTitle:@"退出登录" forState:UIControlStateNormal];
+    
+    logout_btn.titleLabel.font = [UIFont systemFontOfSize:15.0];
+    
+    [logout_btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    
+    [logout_btn setTitleColor:[UIColor grayColor] forState:UIControlStateHighlighted];
+    
+    [self.view addSubview:logout_btn];
+    
 }
 
 - (void)didReceiveMemoryWarning {
